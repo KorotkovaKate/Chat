c975d99cc4fdd367fa4dcd058ba17e5eaf6e3c14
KorotkovaKate <korotkovakate10@gmail.com>
2025-05-11 00:13:38 +0300

    Create migrations and database

--- Chat.Api/Program.cs
+++ Chat.Api/Program.cs
@@ -1,4 +1,12 @@
+using Chat.DAL;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+var configuration = builder.Configuration;
+builder.Services.AddDbContext<ChatDbContext>(options =>
+{
+    options.UseSqlServer(configuration.GetConnectionString(nameof(ChatDbContext)));
+});
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
