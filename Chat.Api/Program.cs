c3129b01adc9c158b31ef0ebe796a7f18007d02b
KorotkovaKate <korotkovakate10@gmail.com>
2025-05-12 19:47:49 +0300

    Add scoped and add swagger

--- Chat.Api/Program.cs
+++ Chat.Api/Program.cs
@@ -1,4 +1,8 @@
+using Chat.Application.Services;
+using Chat.Core.Interfaces.Repositories;
+using Chat.Application.Interfaces.Services;
 using Chat.DAL;
+using Chat.DAL.Repositories;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,8 +11,27 @@ builder.Services.AddDbContext<ChatDbContext>(options =>
 {
     options.UseSqlServer(configuration.GetConnectionString(nameof(ChatDbContext)));
 });
+
+builder.Services.AddScoped<IChatRepository, ChatRepository>();
+builder.Services.AddScoped<IChatService, ChatService>();
+
+builder.Services.AddScoped<IMessageRepository, MessageRepository>();
+builder.Services.AddScoped<IMessageService, MessageService>();
+
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<IUserService, UserService>();
+
+builder.Services.AddControllers();
+
+
+builder.Services.AddSwaggerGen();
+builder.Services.AddEndpointsApiExplorer();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.MapControllers();
 
 app.Run();
